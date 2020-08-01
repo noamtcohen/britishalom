7586cc81f8f1f27bfbfb623992818cc5acfd36e9
britishalom <www.thenextworld.co.il@gmail.com>
2020-08-01 09:52:36 +0300

    gh

--- js/site.js
+++ js/site.js
@@ -2,6 +2,9 @@ $(function () {
     $("#title").load("fragments/title.html");
     $("#Donations").load("fragments/donations.html");
 
+    $("#Donations").click(function (e) {
+        e.stopPropagation();
+    })
     $("body").click(function () {
         $("#Donations").hide();
     });
