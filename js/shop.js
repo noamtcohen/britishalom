102762503cc71110a2f1d32eba26488b9cf107cb
britishalom <www.thenextworld.co.il@gmail.com>
2020-08-09 06:03:51 +0300

    sd

--- js/shop.js
+++ js/shop.js
@@ -8,7 +8,7 @@ $(function(){
             $item.find("#title").load("Products/" + id  + "/Title.txt");
             $item.find("#desc").load("Products/" + id  + "/Desc.txt");
             $item.find("#about").load("Products/" + id  + "/About.txt");
-            $item.find("#cover").attr("src","Products/" + id + "/conver.png");
+            $item.find("#cover").attr("src","Products/" + id + "/cover.png");
             $item.find("#back").attr("src","Products/" + id + "/back.png");
             $("#products").append($item);
         });
