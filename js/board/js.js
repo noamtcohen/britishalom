ca857ef991ca2c3b94cfb8e65a99451999df5226
britishalom <www.thenextworld.co.il@gmail.com>
2020-08-09 09:18:02 +0300

    all most there...

--- js/board/js.js
+++ js/board/js.js
@@ -1,39 +1,29 @@
-$(function(){
+$(init);
+
+function init(){
+    $(".myDiv").off("click");
     $('.myDiv').on('click', handleClick);
+
+    $('.close-icon').removeClass('fullscreen');
 
     $('.myDiv').click(function(e) {
         e = $(e.toElement);
-    if (e.hasClass('fullscreen')) {
-        e.off('click', handleClick);
-    } else {
-        e.on('click', handleClick);
-    }
-    });
-
-    //handle the 'X' to close
-    $('.close-icon').on('click', function(e) {
-        e = $(e.toElement).find(".contentContainer");
-        e.find('.close-icon').hide();
-        //e.find('.contentContainer').html('CLICK!');
-        e.removeClass('fullscreen');
-    });
-
-    function handleClick($e) {
-        $e = $($e.toElement);
-
-        $e.addClass('fullscreen');
-        $e.find('.close-icon').show();
-        $e.find('.close-icon').on('click', function(e) {
-            $e.removeClass('fullscreen');
-            $e.find('.close-icon').hide();
-        });
-        // delay 1s then change the html inside
-        $e.find('.contentContainer').delay(1000)
-            .queue(function(n) {
-            $(this).html($e.find(".contentContainer").html());$e.on('click', handleClick);
-            n();
-            });
-
+        if (e.hasClass('fullscreen')) {
+            e.off('click', handleClick);
+        } else {
+            e.on('click', handleClick);
         }
-});
+    });
+}
 
+function handleClick($e) {
+    $e = $($e.toElement);
+    $e.show();
+    $e.addClass('fullscreen');
+    $e.find('.close-icon').show();
+    $e.find('.close-icon').on('click', function(e) {
+        $e.removeClass('fullscreen');
+        $e.find('.close-icon').hide();
+        setTimeout(init,2000);
+    });
+}                          
